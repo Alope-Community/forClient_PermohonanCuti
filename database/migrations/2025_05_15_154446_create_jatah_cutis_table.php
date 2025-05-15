b40b5a475ec83b525eda4b84e2c1a5aca32b6afa
Fikry Ramadhan <fikryramadhan572@gmail.com>
2025-05-15 22:59:31 +0700

    PMC:01: migration

---
+++ database/migrations/2025_05_15_154446_create_jatah_cutis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jatah_cutis', function (Blueprint $table) {
+            $table->id();
+            $table->integer('users_id');
+            $table->date('tahun');
+            $table->integer('total_jatah');
+            $table->integer('sisa_jatah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jatah_cutis');
+    }
+};
