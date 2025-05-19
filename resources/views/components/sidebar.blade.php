1fc8bb673a7303b1d5589891651a4fed35592a94
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2025-05-19 21:28:22 +0700

    add sidebar and index dashboard

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,120 @@
+<div class="wrapper">
+    <aside id="sidebar">
+        <div class="d-flex" style="padding-top: 20px;">
+            <button class="toggle-btn" type="button">
+                <i class="lni lni-grid-alt"></i>
+            </button>
+            <div class="sidebar-logo">
+                <h1 class="text-white">Brand Name</h1>
+            </div>
+        </div>
+         <hr class="text-white">
+        <ul class="sidebar-nav">
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                     <i class="lni lni-grid-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-users"></i>
+                    <span>Pengguna</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-envelope"></i>
+                    <span>Pengajuan Cuti</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-timer"></i>
+                    <span>Riawayat Pengajuan Cuti</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-comments"></i>
+                    <span>Surat Balasan</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-checkmark-circle"></i>
+                    <span>Verifikasi Cuti</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <hr class="text-white">
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-users"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-exit"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
+        </ul>
+    </aside>
+    <div class="main p-3 ms-3 mt-3">
+        <h1 class="mb-4">Hallo, Admin</h1>
+        <hr class="text-black">
+        <div class="row g-3">
+            <!-- Card 1 -->
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <div class="card-body d-flex justify-content-between align-items-center h-100">
+                        <div>
+                            <div class="fw-medium">Karyawan</div>
+                            <div class="fs-4 fw-bold">150</div>
+                        </div>
+                        <i class="lni lni-users fs-1"></i>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 2 -->
+           <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <div class="card-body d-flex justify-content-between align-items-center h-100">
+                        <div>
+                            <div class="fw-medium">Karyawan</div>
+                            <div class="fs-4 fw-bold">150</div>
+                        </div>
+                        <i class="lni lni-users fs-1"></i>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Card 3 -->
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <div class="card-body d-flex justify-content-between align-items-center h-100">
+                        <div>
+                            <div class="fw-medium">Surat Balasan</div>
+                            <div class="fs-4 fw-bold">150</div>
+                        </div>
+                        <i class="lni lni-users fs-1"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+<script>
+    const hamBurger = document.querySelector(" .toggle-btn");
+
+    hamBurger.addEventListener("click", function() {
+        document.querySelector("#sidebar").classList.toggle("expand");
+    });
+</script>
