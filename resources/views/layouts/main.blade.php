cf6819672772f43e67e67c92b8353cad7a3fc87d
Fikry Ramadhan <fikryramadhan572@gmail.com>
2025-05-20 22:20:32 +0700

    set layouts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,12 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 
-    <!-- Icon dan Bootstrap -->
     <link rel="stylesheet" href="https://cdn.lineicons.com/2.0/LineIcons.min.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!-- ✅ Link ke file CSS kamu -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 </head>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function(){
     Route::redirect('/', 'auth/login');
     Route::get('auth/login',[AuthController::class, 'loginPage'])->name('auth.login');
     Route::post('auth/login',[AuthController::class, 'login'])->name('auth.login.post');
+});
 
+Route::middleware('auth')->group(function(){
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
