459b21d82865f01824dec94e4a93b20e4ce41d70
FirdanAja <159457668+FirdanAja@users.noreply.github.com>
2025-05-21 21:32:43 +0700

    panel user

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function(){
-    Route::redirect('/', 'auth/login');
-    Route::get('auth/login',[AuthController::class, 'loginPage'])->name('auth.login');
-    Route::post('auth/login',[AuthController::class, 'login'])->name('auth.login.post');
 
+Route::middleware('guest')->group(function () {
+    Route::redirect('/', 'auth/login');
+    Route::get('auth/login', [AuthController::class, 'loginPage'])->name('auth.login');
+    Route::post('auth/login', [AuthController::class, 'login'])->name('auth.login.post');
+});
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// });
+Route::get('/dashboard', function () {
+    return view('section.dashboard.index');
+});
+Route::get('/user', function () {
+    return view('section.user.index');
 });
